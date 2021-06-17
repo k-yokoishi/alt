93da3c82997f3b823ef98af15732938e6143a8ab
k-yokoishi <ss.xs.m713@gmail.com>
2021-06-17 20:54:49 +0900

    Setup base style and font-family

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      body: [
+        '"游ゴシック体"',
+        'YuGothic',
+        '"游ゴシック Medium"',
+        '"Yu Gothic Medium"',
+        '"游ゴシック"',
+        '"Yu Gothic"',
+        'sans-serif',
+      ],
+      sans: [
+        '"Josefin Sans"'
+      ]
+    },
+    colors: {
+      black: {
+        DEFAULT: '#1A2C44',
+      },
+      white: {
+        DEFAULT: '#FFFFFF',
+      },
+      gray: {
+        DEFAULT: '#848D9A',
+      },
+    },
+    backgroundColor: {
+      gray: '#E5E5E5',
+      black: '#1A2C44',
+    },
+    extend: {
+      spacing: {
+        xs: '4px',
+        sm: '8px',
+        md: '16px',
+        lg: '24px',
+        xl: '48px',
+      },
+    },
+  },
+}
